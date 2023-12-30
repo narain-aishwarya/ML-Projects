b036d6211bf3c5fdd4073e1de0ad9fe2c55ba92b
Aishwarya Narain <134004148+narain-aishwarya@users.noreply.github.com>
2023-12-31 00:35:08 +0530

    Update insta reach analysis.py

--- Time Series/insta reach analysis.py
+++ Time Series/insta reach analysis.py
@@ -11,3 +11,35 @@ from sklearn.linear_model import PassiveAggressiveRegressor
 
 data = pd.read_csv("/kaggle/input/instagram-reach-analysis-case-study/Instagram_data_by_Bhanu.csv",encoding = 'latin1')
 print(data.head())
+
+data = data.dropna()
+data.info()
+
+plt.figure(figsize=(10,8))
+plt.style.use('fivethirtyeight')
+plt.title("Distribution of Impressions from Home")
+sb.distplot(data['From Home'])
+plt.show()
+
+plt.figure(figsize=(10,8))
+plt.title("Distribution of Impression from Hashtags")
+sb.distplot(data['From Hashtags'])
+plt.show()
+
+plt.figure(figsize=(10, 8))
+plt.title("Distribution of Impression from Explore")
+sb.distplot(data ['From Explore'])
+plt.show()
+
+#Now to look at the percentage of impression from various source
+home = data["From Home"].sum()
+hashtags = data["From Hashtags"].sum()
+comments = data["Comments"].sum()
+other = data["From Other"].sum()
+
+labels = ['From Home','From Hashtags', 'Comments','From Other']
+values = [home, hashtags,comments,other]
+
+fig = px.pie(data, values=values, names=labels, 
+             title= 'Impressions on Instagram Posts From Various Sources',hole=0.5)
+fig.show()
